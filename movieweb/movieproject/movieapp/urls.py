659fb81694469a354641d995529201483cb819d5
unknown <varunmaliyejkal@gmail.com>
2023-10-20 15:39:46 +0530

    added

---
+++ movieweb/movieproject/movieapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import path,include
+from .import views
+app_name='movieapp'
+urlpatterns = [
+    path ('',views.index,name='index'),
+    path('movie/<int:movie_id>/',views.detail,name='detail'),
+    path('add/',views.addmovie,name='addmovie'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>/',views.delete,name='delete'),
+]
